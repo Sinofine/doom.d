1c1e840f7adccf6756376efd4d6906f81498ae61
Sinofine Lotusie <i@sinofine.me>
2024-03-20 21:56:02 +0800

    chore

--- config.el
+++ config.el
@@ -71,14 +71,15 @@ If it's a token, then its treated as a function and enabled. Otherwise, the form
          :action doom/open-private-config)
         ))
 
-(setq doom-font (font-spec :family "Anonymous Pro";; "Iosevka Comfy Motion"
+(setq doom-font (font-spec :family "等距更纱宋体 Slab SC";; "Anonymous Pro"
+                           ;; "Iosevka Comfy Motion"
                            :size 18)
       doom-variable-pitch-font (font-spec :family "Iosevka Comfy Motion Duo"))
 (add-hook! 'doom-load-theme-hook :append
            (set-face-attribute 'default nil :weight 'medium)
   (dolist (charset '(kana han cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
-                      charset (font-spec :family "Noto Serif CJK SC" :height 0.5)))
+                      charset (font-spec :family "等距更纱宋体 Slab SC" )))
   (set-fontset-font (frame-parameter nil 'font) 'emoji (font-spec :family "Segoe UI Emoji")))
 
 ;;(require 'doom-modeline-now-playing)
--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
-       ( chinese +childframe )
+       ;; ( chinese +childframe )
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
